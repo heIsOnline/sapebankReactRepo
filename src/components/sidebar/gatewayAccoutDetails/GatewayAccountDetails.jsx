0d0920df433f0168a62dc15f7b99033b8461864a
username <sahmad12@sapient.com>
2017-08-03 21:19:27 +0530

    globalLeft.css and globalRight.css added

---
+++ src/components/sidebar/gatewayAccoutDetails/GatewayAccountDetails.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import { Col,Row } from 'react-grid-system';
+
+const HeaderComponent = (props) =>(<div></div>);
+
+export default class GatewayAccountDetails extends Component{
+    render(){
+        return(
+           <Col offset={{ md: 1 }}>
+               <div>
+                   
+               </div>
+ 		   </Col>
+        )
+    }
+}
+
--- src/components/sidebar/information/InformationComponent.jsx
+++ src/components/sidebar/information/InformationComponent.jsx
@@ -7,8 +7,8 @@ export default class InformationComponent extends Component{
     render(){
         return(
            <Col offset={{ md: 1 }}>
-            
- 					 </Col>
+                    
+ 			</Col>
         )
     }
 }

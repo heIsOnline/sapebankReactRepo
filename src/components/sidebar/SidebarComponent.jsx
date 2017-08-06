63e6d5070e3d83336521d5a9da7dcbf1ddcd1e60
nikhil seth <nikhilseth@nikhils-MacBook-Air.local>
2017-08-06 16:52:36 +0530

    Updates in global and removing header component

--- src/components/sidebar/SidebarComponent.jsx
+++ src/components/sidebar/SidebarComponent.jsx
@@ -3,16 +3,18 @@ import { Col,Row } from 'react-grid-system';
 import InformationComponent from './information/InformationComponent';
 import GatewayAccountDetails from './gatewayAccoutDetails/GatewayAccountDetails';
 import '../../styles/global.css'
-const HeaderComponent = () =>(<div><h1>sapebank</h1><span>The bank of the moment lorem content</span></div>);
+
 
 export default class SidebarComponent extends Component{
     
     render(){
      
           return(  
-             <Col md={4}>    
+             <Col md={4} bsClass = "secondary-button-color">    
                 <Row>
-                    <HeaderComponent/>
+                    <div>
+                        <h1>sapebank</h1><span>The bank of the moment lorem content</span>
+                    </div>
                 </Row>
                 <Row>
                      <InformationComponent/>
